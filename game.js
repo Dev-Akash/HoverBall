7730e0375ac1efe309774b557b9609ad4836ffae
Akash Srivastava <43779954+Dev-Akash@users.noreply.github.com>
2021-05-16 13:46:53 +0530

    Added collision

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var enemy = document.getElementById("enemy");
 var player = document.getElementById("ball");
+var pointText = document.getElementById("points");
+var livesText = document.getElementById("lives");
 var id = 0;
 var peaked = false;
 var speed = 1;
@@ -7,6 +9,8 @@ var playerBottom = 10;
 var playerState = false;
 var playerId = 0;
 var life = 10;
+var points = 0;
+var collision = false;
 
 // Attack(speed);
 setEventListeners();
@@ -14,11 +18,22 @@ setEventListeners();
 
 function Start(){
 		if(life != 0){
-			Attack();
-			life -= 1;
-			setTimeout(Start, 2000);
+			Attack(30);
+			if (collision) {
+				points += 1;
+			}
+			else{
+				life -= 1;
+			}
+
+			pointText.style.innerHTML = points;
+			livesText.style.innerHTML = life;
+			console.log(life, points);
+			collision = false;
+			setTimeout(Start, 3000);
 		}
 		else{
+			//gameover UI
 		}
 }
 
@@ -26,6 +41,7 @@ function Attack(speed){
 	enemy.style.left = '0%';
 	enemy.style.top = '70%';
 	peaked = false;
+	enemy.style.display = "block";
 	id = setInterval(move, speed);
 }
 
@@ -54,6 +70,16 @@ function move(){
 		}
 		else{
 			enemy.style.top = (currentTop-1)+ '%';
+		}
+
+		if(currentLeft < 55 && currentLeft > 45 && playerBottom > 60 && playerBottom < 80){
+			// collision occurred
+			collision = true;
+			enemy.style.display = "none";
+			//Todo: add destroy animation.
+		}
+		else{
+			//no collision
 		}
 	}
 }
